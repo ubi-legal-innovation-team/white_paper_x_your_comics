cb7f4e7af11707006ca4c889d5bf78b69df10527
Arthur <a.dhuy@orange.fr>
2021-11-02 18:58:42 +0100

    fix scss error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,24 +3,24 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-// ----------------------------------------------------
-// Note(Legal lab): ABOVE IS RAILS DEFAULT CONFIGURATION
-// WRITE YOUR OWN JS STARTING FROM HERE 👇
-// ----------------------------------------------------
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+// // ----------------------------------------------------
+// // Note(Legal lab): ABOVE IS RAILS DEFAULT CONFIGURATION
+// // WRITE YOUR OWN JS STARTING FROM HERE 👇
+// // ----------------------------------------------------
 
-// External imports
+// // External imports
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // yourMethod();
-});
+// // Internal imports, e.g:
+// // import { initSelect2 } from '../components/init_select2';
+// document.addEventListener('turbolinks:load', () => {
+//   // Call your functions here, e.g:
+//   // yourMethod();
+// });
