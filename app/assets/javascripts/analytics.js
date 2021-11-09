bf519f6ee8fd0f9e799b273fbfc8a0c49d242108
Arthur <a.dhuy@orange.fr>
2021-11-09 16:27:27 +0100

    GA done

---
+++ app/assets/javascripts/analytics.js
@@ -0,0 +1,15 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments) };
+
+gtag('js', new Date());
+
+const trackGoogleAnalytics = (event) => {
+  gtag('config', 'G-NEW605NMVF', {
+    'cookie_flags': 'max-age=7200;secure;samesite=none',
+    page_location: event.data.url,
+    page_path: event.srcElement.location.pathname,
+    page_title: event.srcElement.title
+  });
+};
+
+document.addEventListener('turbolinks:load', trackGoogleAnalytics);
