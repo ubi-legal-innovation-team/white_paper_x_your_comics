127b95aca2538ad8adce3239ad2bbe5bdc182f75
Arthur <a.dhuy@orange.fr>
2021-12-09 15:31:53 +0100

    update for Heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module WhitePaperXYourComics
       generate.helper false
       generate.test_framework :test_unit, fixture: false
     end
+    config.assets.initialize_on_precompile = false
+    
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
