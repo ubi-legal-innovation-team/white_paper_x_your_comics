12e5d35986faf2f63946dd0b11f673a0b0e79cf2
Arthur <a.dhuy@orange.fr>
2022-03-29 14:35:07 +0200

    remove duplicates method

--- app/models/requester.rb
+++ app/models/requester.rb
@@ -6,11 +6,17 @@ class Requester < ApplicationRecord
 	validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: I18n.t('join.error_message.email') }
 	validates :consent, presence: { message: I18n.t('join.error_message.consent') }
 
-	after_create :set_data_retention_period
+	after_create  :set_data_retention_period
+	before_create :remove_unpermited
 
 	private
 
 	def set_data_retention_period
 		self.update(data_retention_period:self.created_at + 3.year)
 	end
+
+	def remove_unpermited
+		self.destroy if self.email.include?('sample')
+	end
+
 end
--- app/models/user_agent.rb
+++ app/models/user_agent.rb
@@ -1,5 +1,18 @@
 class UserAgent < ApplicationRecord
 	strip_attributes
 
-	validates :host_name, :operating_system, presence:true
+	validates :operating_system, presence: true
+	validates :host_name, :publics_ip, uniqueness: true
+	
+	private
+
+	def self.remove_duplicates
+		grouped = UserAgent.all.group_by{ |user_agent| [user_agent.host_name, user_agent.publics_ip] }
+		p grouped
+		grouped.values.each do |duplicates|
+			first_one = duplicates.shift
+
+			duplicates.each{|double| double.destroy}
+		end
+	end
 end
